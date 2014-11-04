7c1635c1e3b5b9da3daeec1b6dd0abc058f98d5d
Dylan Marriott <info@d-32.com>
2014-11-03 20:34:09 -0500

    Added documentation

--- Pod/Classes/DMJob.h
+++ Pod/Classes/DMJob.h
@@ -8,12 +8,32 @@
 
 typedef void (^CompletionBlock)(BOOL);
 
+/**
+ *  Defines what a job is.
+ */
 @protocol DMJob <NSObject, NSCoding>
 
+/**
+ *  This method will be called by the JobManager.
+ *  Inside here you should execute your desired work.
+ *  When done, call the completion block with either YES (success) or NO (fail -> retry)
+ *
+ *  @param completion completion block
+ */
 - (void)executeWithCompletion:(CompletionBlock)completion;
 
 @optional
+
+/**
+ *  The desired delay until the job will be executed again if it fails .
+ *
+ *  @return delay in seconds, default is 10
+ */
 - (NSTimeInterval)retryDelay;
+
+/**
+ *  This method will be called before the JobManager executes the job again. Use this if you want to increase the retryDelay according to the number of retries.
+ */
 - (void)willRetry;
 
 @end
